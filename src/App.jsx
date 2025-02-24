21cca12fa900573b3d4dfa3a3af3b08a9f604822
subinsong01 <haely97@naver.com>
2025-02-24 12:22:59 +0900

    style: 전체 레이아웃 설정

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,19 @@ import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import NotFound from './pages/NotFound';
-import emotion1 from './assets/emotion1.png';
-import emotion2 from './assets/emotion2.png';
-import emotion3 from './assets/emotion3.png';
-import emotion4 from './assets/emotion4.png';
-import emotion5 from './assets/emotion5.png';
+import { getEmotionImage } from './util/get-emotion-image';
 
 function App() {
   return (
     <>
       <div>
-        <img src={emotion1} alt="좋아요" />
-        <img src={emotion2} alt="괜찮아요" />
-        <img src={emotion3} alt="그냥그래요" />
-        <img src={emotion4} alt="별로네요" />
-        <img src={emotion5} alt="싫어요" />
+        <img src={getEmotionImage(1)} alt="좋아요" />
+        <img src={getEmotionImage(2)} alt="괜찮아요" />
+        <img src={getEmotionImage(3)} alt="그냥그래요" />
+        <img src={getEmotionImage(4)} alt="별로네요" />
+        <img src={getEmotionImage(5)} alt="싫어요" />
       </div>
-      
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/new" element={<New />} />
