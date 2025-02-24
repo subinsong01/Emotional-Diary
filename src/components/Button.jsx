4b81fb95e9e441ea8580f69b70f3f5c374b24ab3
subinsong01 <haely97@naver.com>
2025-02-24 14:38:23 +0900

    refactor: 코드 구조 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import './Button.css';
 
 const Button = ({text, type, onClick}) => {
   return (
-  <button onClick={onClick} type={`Button Button_${type}`}className='Button'>
+  <button onClick={onClick} className='Button'>
     {text}
   </button>
-)};
+  )};
 export default Button;
