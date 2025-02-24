c50a2e21086837e46ce899fca176583b49f78c92
subinsong01 <haely97@naver.com>
2025-02-24 17:11:14 +0900

    feat: 헤더에 월 설정 기능 삽입

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+
 const Home = () => {
-  return<div>home</div>
-}
+
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  return(
+    <>
+    <header>
+      <Header 
+        title={`${pivotDate.getFullYear()}년 
+        ${pivotDate.getMonth() + 1 }월`}
+        leftChild={<Button text={"<"} onClick={onDecreaseMonth}/>}
+        rightChild={<Button text={">"} onClick={onIncreaseMonth}/>}
+      />
+    </header>
+      <DiaryList />
+    </>
+  );
+};
 
 export default Home;
