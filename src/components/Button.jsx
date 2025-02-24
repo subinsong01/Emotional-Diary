ffb6ae9ae9f2614cd855f3125d04497361915294
subinsong01 <haely97@naver.com>
2025-02-24 17:10:30 +0900

    fix: button type props로 넘기기 설정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import './Button.css';
 
 const Button = ({text, type, onClick}) => {
   return (
-  <button onClick={onClick} className='Button'>
+  <button onClick={onClick} className={`Button Button_${type}`}>
     {text}
   </button>
   )};
