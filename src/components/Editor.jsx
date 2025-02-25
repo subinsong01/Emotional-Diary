0765418561b415441d877109a95999582b5f3f84
subinsong01 <haely97@naver.com>
2025-02-25 17:46:08 +0900

    feat: 일기 작성하기 페이지에 날짜입력 기능 완료

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,7 @@
 import './Editor.css';
 import EmotionItem from './EmotionItem';
 import Button from './Button';
+import { useState } from 'react';
 
 const EmotionList = [
   {
@@ -24,14 +25,51 @@ const EmotionList = [
     emotionName: '끔찍'
   },
 ];
+
+const getStringedDate = (targetDate) => {
+  //날짜 -> YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let day = targetDate.getDate();
+
+  if(month < 10){
+    month = `0${month}`;
+  }
+  if(day < 10){
+    day = `0${day}`;
+  }
+
+  return `${year} ${month} ${day}`;
+};
 const Editor = () => {
+  const [input, setInput] = useState({
+    createdDate : new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  const onChangeInput = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    let name = e.target.name;
+    let value = e.target.value;
+    
+    if(name === 'createdDate'){
+      value = new Date(value);
+    }
+    setInput({
+      ...input,
+      [name]: value,
+    })
+  }
   const emotionId = 3;
 
   return(
     <div className='Editor'>
       <section className='date_section'>
         <h4>오늘의 날짜</h4>
-        <input type='date' />
+        <input  name="createdDate" onChange={onChangeInput} defaultValue={getStringedDate(input.createdDate)} type='date' />
       </section>
       <section className='emotion_section'>
         <h4>오늘의 감정</h4>
