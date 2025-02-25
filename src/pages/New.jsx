61ea21de70c259db72a8667adbada89031b6f465
subinsong01 <haely97@naver.com>
2025-02-25 17:25:28 +0900

    feat: 일기 작성하기 뒤로가기 기능 적용

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,12 +1,15 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useNavigate } from 'react-router-dom';
 const New = () => {
+  const navigate = useNavigate();
+
   return(
   <div>
     <header>
       <Header title={"새 일기 작성하기"}
-      leftChild={<Button text={"< 뒤로가기"}/>} />
+      leftChild={<Button onClick={() => navigate(-1)} text={"< 뒤로가기"}/>} />
     </header>
     <Editor />
   </div>
