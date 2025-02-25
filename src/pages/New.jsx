29af61ec37d5508a533d6cc647d4b8b94dea6ff9
subinsong01 <haely97@naver.com>
2025-02-25 15:25:30 +0900

    feat: 새 일기 작성하기 헤더 완성

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,14 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
 const New = () => {
-  return<div>new</div>
+  return(
+  <div>
+    <header>
+      <Header title={"새 일기 작성하기"}
+      leftChild={<Button text={"< 뒤로가기"}/>} />
+    </header>
+  </div>
+  )
 }
 
 export default New;
