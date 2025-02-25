c020f7a8a3e623a3bcfbea862ee9c07a749d22fd
subinsong01 <haely97@naver.com>
2025-02-25 17:16:34 +0900

    style: 일기 작성하기 UI 구현 완료

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
+import Editor from "../components/Editor";
 const New = () => {
   return(
   <div>
@@ -7,6 +8,7 @@ const New = () => {
       <Header title={"새 일기 작성하기"}
       leftChild={<Button text={"< 뒤로가기"}/>} />
     </header>
+    <Editor />
   </div>
   )
 }
